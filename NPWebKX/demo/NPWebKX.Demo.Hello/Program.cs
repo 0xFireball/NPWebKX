d9451191e6c72d602e604eaed3dac078532e3c30
0xFireball <0xFireball@outlook.com>
2017-02-03 17:40:47 -0800

    add jsdom script

---
+++ NPWebKX/demo/NPWebKX.Demo.Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using NPWebKX.JS;
+
+namespace NPWebKX.Demo.Hello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var jsdomscript = JSDomLoader.LoadJSDomScriptSource();
+
+        }
+    }
+}
---
+++ NPWebKX/src/NPWebKX/JS/JSDomLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace NPWebKX.JS
+{
+    public class JSDomLoader
+    {
+        public static Stream LoadJSDomScriptStream()
+        {
+            return typeof(JSDomLoader).GetTypeInfo().Assembly.GetManifestResourceStream("JS.jsdom_nkx.min.js");
+        }
+
+        public static string LoadJSDomScriptSource()
+        {
+            string result;
+            using (var sr = new StreamReader(LoadJSDomScriptStream()))
+            {
+                result = sr.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
